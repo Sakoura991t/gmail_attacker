895ad57bed4ac7a063dbcfeb912339e4cbfc20e1
Sakoura991t <149597163+Sakoura991t@users.noreply.github.com>
2026-01-08 19:42:55 +0400

    Update author name in gmail_attacker.py

--- gmail_attacker.py
+++ gmail_attacker.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
-'''coded by ayoub sirai'''
+'''coded by ibra zebra'''
 
 import smtplib
 from os import system
 
 def main():
    print '================================================='
-   print '               coded by ayoub sirai              '
+   print '               coded by ibra zebra'
    print '================================================='
    print '               ++++++++++++++++++++              '
    print '\n                                               '
